6a839a40929e23b0cc3b06973b1ee1781bedb9cd
あ〜 <52856732+03gs@users.noreply.github.com>
2019-08-25 00:02:52 +0900

    v1.0

--- lineclient.py
+++ lineclient.py
@@ -1,13 +1,10 @@
-import requests, json, os
-
-import thriftpy2
+import requests, rsa, thriftpy2
 from thriftpy2.http import THttpClient
 from thriftpy2.protocol import TCompactProtocol
 from thriftpy2.thrift import TClient
 
 class Config(object):
     HOST = "https://legy-jp.line.naver.jp:443"
-
 
     # Client
     LA = "IOSIPAD\t9.12.0\tiOS\t12.4"
@@ -25,7 +22,6 @@ class Config(object):
 
 
 class LINE(object):
-    
     def __init__(self):
         self.thrift = thriftpy2.load("line.thrift")
 
@@ -40,16 +36,31 @@ class LINE(object):
         self.protocol = TCompactProtocol(self.transport)
         self.transport.open()
         self.client = TClient(self.thrift.Service, self.protocol)
+        self.certificate = None
 
 
-    def loginWithQrCode(self, keepLoggedIn=True, systemName=None, appName=None):
-        if systemName is None:
-            systemName = Config.SYSTEM_NAME
-        if appName is None:
-            appName = Config.LA
+    def __loadSession(self):
+    	# Talk Service
+        self.transport.setCustomHeaders({
+            "User-Agent": Config.UA,
+            "X-Line-Carrier": Config.CARRIER,
+            "X-Line-Application": Config.LA,
+            "X-Line-Access": self.authToken
+        })
+
+        self.transport.path = Config.LINE_API_QUERY_PATH_FIR
+        self.talk = TClient(self.thrift.Service, self.protocol)
+        self.profile = self.talk.getProfile()
+        print("[Login success] "+ self.profile.displayName)
+
+        self.transport.path = Config.LONG_POLLING
+        self.poll = TClient(self.thrift.Service, self.protocol)
+        self.revision = self.poll.getLastOpRevision()
+
+    def loginWithQrCode(self):
 
         self.transport.path = Config.AUTH_QUERY_PATH
-        res = self.client.getAuthQrcode(True, systemName)
+        res = self.client.getAuthQrcode(True, Config.SYSTEM_NAME)
         print("line://au/q/" + res.verifier)
 
         headers = {
@@ -67,38 +78,96 @@ class LINE(object):
             LR = self.thrift.LoginRequest()
             LR.type = self.thrift.LoginType.QRCODE
             LR.identityProvider = self.thrift.IdentityProvider.LINE
-            LR.keepLoggedIn = keepLoggedIn
+            LR.keepLoggedIn = True
             LR.accessLocation = Config.IP_ADDR
             LR.verifier = verifier
             LR.e2eeVersion = 1
+            result = self.client.loginZ(LR)
         except:
             raise Exception('Login failed')
 
-        result = self.client.loginZ(LR)
         if result.type == LoginResultType.SUCCESS:
             if result.authToken is not None:
-                self.loginWithAuthToken(result.authToken, appName)
+                self.loginWithAuthToken(result.authToken)
             else:
                 return False
         else:
             raise Exception('Login failed')
-        print(result)
 
 
-    def loginWithAuthToken(self, authToken=None, appName=None):
+    def loginWithCredential(self, mail, password, certificate=None):
+
+        self.transport.path = Config.AUTH_QUERY_PATH
+        RSAKey = self.client.getRSAKeyInfo(self.thrift.IdentityProvider.LINE)
+        message = (chr(len(RSAKey.sessionKey))+RSAKey.sessionKey+chr(len(mail))+mail+chr(len(password))+password).encode('utf-8')
+        pub_key = rsa.PublicKey(int(RSAKey.nvalue, 16), int(RSAKey.evalue, 16))
+        crypto = rsa.encrypt(message, pub_key).hex()
+
+        try:
+            with open(mail + '.crt', 'r') as f:
+                self.certificate = f.read()
+        except:
+            if certificate is not None:
+                self.certificate = certificate
+
+        LR = self.thrift.LoginRequest()
+        LR.type = self.thrift.LoginType.ID_CREDENTIAL
+        LR.identityProvider = self.thrift.IdentityProvider.LINE
+        LR.identifier = RSAKey.keynm
+        LR.password = crypto
+        LR.keepLoggedIn = True
+        LR.accessLocation = Config.IP_ADDR
+        LR.systemName = Config.SYSTEM_NAME
+        LR.certificate = self.certificate
+        LR.e2eeVersion = 1
+
+        self.transport.path = Config.AUTH_REGISTRATION
+        result = self.client.loginZ(LR)
+        
+        if result.type == self.thrift.LoginResultType.REQUIRE_DEVICE_CONFIRM:
+            print("Enter pincode: "+result.pinCode)
+
+            headers = {
+                "User-Agent": Config.UA,
+                "X-Line-Application": Config.LA,
+                "X-Line-Carrier": Config.CARRIER,
+                "X-Line-Access": result.verifier
+            }
+            verifier = requests.get(Config.HOST + Config.LINE_CERTIFICATE_PATH, headers=headers).json()["result"]["verifier"]
+
+            try:
+                LR = self.thrift.LoginRequest()
+                LR.type = self.thrift.LoginType.QRCODE
+                LR.keepLoggedIn = True
+                LR.verifier = verifier
+                LR.e2eeVersion = 1
+                result = self.client.loginZ(LR)
+            except:
+                raise Exception('Login failed')
+            
+            if result.type == self.thrift.LoginResultType.SUCCESS:
+                if result.certificate is not None:
+                    with open(mail + '.crt', 'w') as f:
+                        f.write(result.certificate)
+                    self.certificate = result.certificate
+                if result.authToken is not None:
+                    self.loginWithAuthToken(result.authToken)
+                else:
+                    return False
+            else:
+                raise Exception('Login failed')
+
+        elif result.type == self.thrift.LoginResultType.REQUIRE_QRCODE:
+            self.loginWithQrCode()
+            pass
+
+        elif result.type == self.thrift.LoginResultType.SUCCESS:
+            self.certificate = result.certificate
+            self.loginWithAuthToken(result.authToken)
+
+    def loginWithAuthToken(self, authToken=None):
         if authToken is None:
             raise Exception('Please provide Auth Token')
-        if appName is None:
-            appName = Config.LA
-        self.transport.setCustomHeaders({
-            "User-Agent": Config.UA,
-            "X-Line-Carrier": Config.CARRIER,
-            "X-Line-Application": Config.LA,
-            "X-Line-Access": authToken
-        })
-        self.protocol = TCompactProtocol(self.transport)
-        self.transport.open()
-        self.client = TClient(self.thrift.Service, self.protocol)
-        self.transport.path = Config.LINE_API_QUERY_PATH_FIR
+
         self.authToken = authToken
-        print(self.client.getProfile())
+        self.__loadSession()
--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 from lineclient import LINE
 
 line = LINE()
-line.loginWithQrCode()
+
+#line.loginWithQrCode()
+#line.loginWithAuthToken("{AUTH_TOKEN}")
+
+#line.loginWithCredential("xvideos@gmail.com", "114514_810", "{CERT_TEXT}")
+
+#line.loginWithCredential("xvideos@gmail.com", "114514_810")
